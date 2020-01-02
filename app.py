ee7b0788f68c787f9c3e75264c49e66ff4428b02
vic30004 <victor_993@msn.com>
2020-01-01 19:23:44 -0800

    added logic to make sure there was an error with text

--- app.py
+++ app.py
@@ -13,8 +13,14 @@ def get_word(w):
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
 
+        if yn.lower()=="y":
+              return data[get_close_matches(w, data.keys())[0]]
+        elif yn.lower()=='n':
+              return "Ooops something went wrong. Make sure your text is correct"
+        else: 
+              return "I didn't get that, please try again"
     else:
-        return "Ooops something went wrong. Make sure your text is corrext"
+        return "Ooops something went wrong. Make sure your text is correct"
 
 word = input("Enter word: ")
 
