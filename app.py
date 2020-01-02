2a7118a2af4abf0caf6477e0764ed5ad987018fb
vic30004 <victor_993@msn.com>
2020-01-01 19:16:41 -0800

    checks for the closest match

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import json 
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
 
-print(data['rain'])
 
 
-def get_word():
-  userInp = str(input("What word are you looking for ?"))
-  try:
-    for word in data[userInp]:
-            print(f"{userInp}: {word}")
-  except:
-      print("oops, something went wrong. Make sure your text is correct!")
 
-get_word()
+def get_word(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+
+    else:
+        return "Ooops something went wrong. Make sure your text is corrext"
+
+word = input("Enter word: ")
+
+print(get_word(word))
