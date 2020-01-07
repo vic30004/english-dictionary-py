ef3ba15f9dcdf5485b0123d9af628e1abc732ab2
vic30004 <victor_993@msn.com>
2020-01-07 14:04:02 +0200

    completed the logic for the sql dictionary

--- sql-dictionary/app.py
+++ sql-dictionary/app.py
@@ -9,11 +9,23 @@ database="ardit700_pm1database"
 
 cursor = con.cursor()
 
-query = cursor.execute("SELECT * FROM Dictionary")
-result = cursor.fetchall()
+def getDefinition():
+    ProbUser = input("Would you like to look for a word. type Y for yes and N for no ")
+    while ProbUser.lower()=="y":
+        wordToLookUp= str(input("What word would you like to look up?"))
+        query = cursor.execute(f"SELECT * FROM Dictionary WHERE Expression ='{wordToLookUp}'")
+        result = cursor.fetchall()
+        if result:
+            for item1,item2 in result:
+                print(f"{item1}: {item2}")
+        else: 
+            print("Sorry we couldn't find that. Please try again")
+        ProbUser = input("Would you like to look for a word. type Y for yes and N for no ")
 
-for item1,item2 in result:
-    print(item1,item2)
+    else:
+        print("We are sorry to see you go. Thank you for using us.")
+        return
 
+getDefinition()
 
 
